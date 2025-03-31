6945d9d1f06c4bf20d3a61baf08a653a4f009cb8
Tim Herrin <Teffluvium@gmail.com>
2025-03-31 13:05:51 -0700

    Lots of modifications of pi_hole_display.py
    * Modularization
    * Typehinting
    * Formatting
    * Automation of text updates via dictionary and itertools.cylcle()
    * Accesses PiHole statistics via API

--- my_pi_hole/pi_hole_display.py
+++ my_pi_hole/pi_hole_display.py
@@ -1,25 +1,28 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
-import json
+import itertools
 import subprocess
 import time
 from enum import Enum, auto
 
 import board
 import digitalio
-import requests
 from adafruit_rgb_display import st7789
 from pihole6api import PiHole6Client
 from PIL import Image, ImageDraw, ImageFont
 
 API_URL = "http://localhost"
-API_TOKEN = "2A+EambjgEiPTnfo1nodZ5nagEBm++MHXFVQZKMl8vE="
+API_TOKEN = r"mrukyqgwUoCMuSvDyacy5VS9/J0HQxgyHnhUUnB+32A="
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 CS_PIN = digitalio.DigitalInOut(board.CE0)
 DC_PIN = digitalio.DigitalInOut(board.D25)
 RESET_PIN = None
+
+# Configuration for buttons A and B
+BUTTON_A_PIN = board.D23
+BUTTON_B_PIN = board.D24
 
 # Config for display baudrate (default max is 24mhz):
 BAUDRATE = 64_000_000
@@ -38,13 +41,12 @@ DISPLAY_X_OFFSET = 0
 DISPLAY_Y_OFFSET = 80
 ROTATION = 0  # 180
 
-
 # Image frame
 FONT_TYPE = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
 FONT_SIZE = 16
 
 
-def initialize_display():
+def initialize_display() -> digitalio.DigitalInOut:
     # Setup SPI bus using hardware SPI:
     spi = board.SPI()
 
@@ -64,22 +66,40 @@ def initialize_display():
     return disp
 
 
-def c_to_f(temp: float):
+def initialize_buttons() -> tuple[digitalio.DigitalInOut, digitalio.DigitalInOut]:
+    # Setup of buttons A and B
+    buttonA = digitalio.DigitalInOut(BUTTON_A_PIN)
+    buttonB = digitalio.DigitalInOut(BUTTON_B_PIN)
+    buttonA.switch_to_input()
+    buttonB.switch_to_input()
+
+    return buttonA, buttonB
+
+
+def initialize_backlight() -> digitalio.DigitalInOut:
+    # Turn on the backlight
+    backlight = digitalio.DigitalInOut(board.D22)
+    backlight.switch_to_output()
+    backlight.value = False
+
+    return backlight
+
+
+def c_to_f(temp: float) -> float:
     # Convert Celcius to Fahrenheit
     return (temp * 9 / 5) + 32
 
 
-def get_stats():
+def get_system_stats() -> dict[str, str]:
     # CLI commands to get system information
     cmd = {
         "IP": "hostname -I | cut -d' ' -f1",
         "HOST": "hostname | tr -d '\\n'",
-        "CPU": "top -bn1 | " "grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'",
+        "CPU": "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'",
         "MemUsage": [
-            "free -m | "
-            "awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'",
+            "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'",
         ],
-        "Disk": "df -h | " 'awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\'',
+        "Disk": 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\'',
         "Temp": [
             "cat /sys/class/thermal/thermal_zone0/temp | "
             "awk '{printf \"%.5f\", $(NF-0) / 1000}'",
@@ -93,35 +113,43 @@ def get_stats():
     }
 
     # Add labels to some fields
-    stats["IP"] = f'IP: {stats.get("IP").strip()}'
-    stats["HOST"] = f'HOST: {stats.get("HOST").strip()}'
+    stats["IP"] = f"IP: {stats.get('IP').strip()}"
+    stats["HOST"] = f"HOST: {stats.get('HOST').strip()}"
 
     # Convert the temperature to Fahrenheit
-    stats["Temp"] = f'CPU Temp: {c_to_f(float(stats.get("Temp"))):.1f} F'
+    stats["Temp"] = f"CPU Temp: {c_to_f(float(stats.get('Temp'))):.1f} F"
 
     return stats
 
 
-def get_pihole_stats(client):
-    # # Pi Hole API data!
-    # r = requests.get(API_URL)
-    # data = json.loads(r.text)
-    # _ = [print(f"{k}: {v}") for k,v in data.items()]
-    # print()
-    # #DNSQUERIES = data['dns_queries_today']
-    # #ADSBLOCKED = data['ads_blocked_today']
-    # #CLIENTS = data['unique_clients']
+def get_pihole_stats(client: PiHole6Client) -> dict[str, str]:
+    # Gather summary stats for the PiHole
+    stats_summary = client.metrics.get_stats_summary()
+    total_queries = stats_summary.get("queries").get("total")
+    blocked_queries = stats_summary.get("queries").get("blocked")
+    percent_queries = stats_summary.get("queries").get("percent_blocked")
 
-    current_time = int(time.time())
-    twenty_four_hours_ago = current_time - (24 * 3600)
+    # Get current list of clients served by PiHole
+    client_dict = client.client_management.get_clients()
+    client_list = client_dict.get("clients")
 
-    print(f"Current Unix timestamp:      {current_time}")
-    print(f"Unix timestamp 24 hours ago: {twenty_four_hours_ago}")
+    stats = {
+        "Client Count": f"Number of Clients: {len(client_list):}",
+        "Total Queries": f"Total Queries: {total_queries:,}",
+        "Blocked Queries": f"Blocked Queries: {blocked_queries:,}",
+        "Percent Blocked": f"Percent Blocked: {percent_queries:.1f}%",
+    }
 
-    pass
+    return stats
 
 
-def update_frame_text(draw, font, stats, x, top):
+def update_frame_text(
+    draw: ImageDraw,
+    font: ImageFont.FreeTypeFont,
+    stats: dict[str, str],
+    x: int,
+    top: int,
+) -> None:
     # Determine the vertical text separation for this font
     text = "Temp Text"
     bbox = draw.textbbox((0, 0), text, font=font)
@@ -129,23 +157,24 @@ def update_frame_text(draw, font, stats, x, top):
     y_offset = bbox[3] - bbox[1]
     y_offset += 7
 
+    color_list = [
+        "#FFFFFF",  # White
+        "#FFFF00",  # Yellow
+        "#00FF00",  # Green
+        "#00FFFF",  # Cyan
+        "#FF00FF",  # Magenta
+        # "#0000FF",  # Blue (a bit too dark on black background)
+    ]
+    color_cycle = itertools.cycle(color_list)
+
     # Write four lines of text.
     y = top
-    # draw.text((x, y), IP, font=font, fill="#FFFFFF")
-    draw.text((x, y), stats.get("IP"), font=font, fill="#FFFFFF")
-    y += y_offset
-    draw.text((x, y), stats.get("HOST"), font=font, fill="#FFFFFF")
-    y += y_offset
-    draw.text((x, y), stats.get("CPU"), font=font, fill="#FFFF00")
-    y += y_offset
-    draw.text((x, y), stats.get("MemUsage"), font=font, fill="#00FF00")
-    y += y_offset
-    draw.text((x, y), stats.get("Disk"), font=font, fill="#0000FF")
-    y += y_offset
-    draw.text((x, y), stats.get("Temp"), font=font, fill="#FF00FF")
+    for val in stats.values():
+        draw.text((x, y), val, font=font, fill=next(color_cycle))
+        y += y_offset
 
 
-def initialize_image(disp):
+def initialize_image(disp: st7789.ST7789) -> Image.Image:
     # Create blank image for drawing.
     # Make sure to create image with mode 'RGB' for full color.
     height = disp.width  # we swap height/width to rotate it to landscape!
@@ -162,7 +191,9 @@ class ButtonState(Enum):
     NONE = auto()
 
 
-def get_button_states(buttonA, buttonB):
+def get_button_states(
+    buttonA: digitalio.DigitalInOut, buttonB: digitalio.DigitalInOut
+) -> ButtonState:
     # Return the state of the button presses:
     #
     if buttonA.value and not buttonB.value:  # just button A pressed
@@ -176,16 +207,14 @@ def get_button_states(buttonA, buttonB):
 
 
 def main():
-    # client = PiHole6Client(API_URL, API_TOKEN)
+    # Establish a connection to the PiHole API
+    client = PiHole6Client(API_URL, API_TOKEN)
 
+    # Initialize the hardware
     disp = initialize_display()
-
-    buttonA = digitalio.DigitalInOut(board.D23)
-    buttonB = digitalio.DigitalInOut(board.D24)
-    buttonA.switch_to_input()
-    buttonB.switch_to_input()
-
+    (buttonA, buttonB) = initialize_buttons()
     image = initialize_image(disp)
+    backlight = initialize_backlight()
 
     # Get drawing object to draw on image.
     draw = ImageDraw.Draw(image)
@@ -198,7 +227,7 @@ def main():
     # First define some constants to allow easy resizing of shapes.
     padding = -2
     top = padding
-    bottom = disp.height - padding
+    # bottom = disp.height - padding
     # Move left to right keeping track of the current x position for drawing shapes.
     x = 0
 
@@ -207,33 +236,32 @@ def main():
     # Some other nice fonts to try: http://www.dafont.com/bitmap.php
     font = ImageFont.truetype(FONT_TYPE, FONT_SIZE)
 
-    # Turn on the backlight
-    backlight = digitalio.DigitalInOut(board.D22)
-    backlight.switch_to_output()
-    backlight.value = False
-
     try:
         while True:
             state = get_button_states(buttonA, buttonB)
+            stats_dict = {}
             if state == ButtonState.ONLY_A:
                 print("Button A pressed")
+                backlight.value = True  # turn on backlight
+                stats_dict = get_system_stats()
 
             if state == ButtonState.ONLY_B:
                 print("Button B pressed")
+                backlight.value = True  # turn on backlight
+                stats_dict = get_pihole_stats(client)
 
             if state == ButtonState.BOTH:
                 print("Buttons A and B pressed")
-                backlight.value = True  # turn on backlight
+                backlight.value = False  # turn off backlight
 
             if state == ButtonState.NONE:
+                # print("No buttons pressed")
                 backlight.value = False  # turn off backlight
 
             # Draw a black filled box to clear the image.
             draw.rectangle((0, 0, disp.width, disp.height), outline=0, fill=0)
 
-            stats = get_stats()
-
-            update_frame_text(draw, font, stats, x, top)
+            update_frame_text(draw, font, stats_dict, x, top)
 
             # Display image.
             disp.image(image, ROTATION)
@@ -244,6 +272,10 @@ def main():
         print(f"Exiting {__file__}")
         print()
         # Turn off the backlight when Ctrl-C is pressed
+        backlight.value = False
+
+    except:
+        # Turn off the backlight before exiting
         backlight.value = False
 
 
